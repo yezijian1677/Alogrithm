efce7c994fe757a9607a64429a715efd8ff8ae41
yezijian1677 <1677390657@qq.com>
2019-03-14 19:43:31 +0800

    3wayquicksort

--- quickSort/quickSort3Way.java
+++ quickSort/quickSort3Way.java
@@ -1,52 +1,48 @@
 package quickSort;
 
+import InsertSort.InsertSort;
+
 public class quickSort3Way {
     //从0到n进行快速排序
     private static void _quickSort(int[] arr, int l, int r){
         if (l>=r){
             return;
         }
-        int partial = partial(arr, l, r);
-        _quickSort(arr, l, partial-1);
-        _quickSort(arr, partial+1, r);
-
-    }
-
-    /**
-     * 双路法解决重复序列的排序
-     * i指向小于元素v的下一个值
-     * j指向大于v元素的上一个值
-     * @param arr
-     * @param l
-     * @param r
-     * @return
-     */
-    private static int partial(int[] arr, int l, int r){
-        swap(arr, l , (int)Math.random()*(r-l+1)+l);
-        //让第一个元素作为参照
-        int v = arr[l];
-        //设置j从l开始
-        int i = l+1;
-        int j = r;
-
-        while (true){
-            while (i<=r&&arr[i]<v){
-                i++;
-            }
-            while (j>=l+1&&arr[j]>v){
-                j--;
-            }
-            if (i>j){
-                break;
-            }
-            swap(arr, i, j);
-            i++;
-            j--;
+        if(r-l<=12){
+            InsertSort.InsertSort(arr);
         }
-        swap(arr, l, j);
+        /**
+         * 开始三路快速排序
+         * lt 从 l开始，标识小于v的值最后一个位置
+         * gt从r+1开始，标识于大于v的最后一个位置
+         * i当前所指
+         */
+       int lt = l;
+       int gt = r+1;
+       int i = l+1;
+       int v = arr[l];
+       while (i<gt){
+           if (arr[i]<v){
+               swap(arr , i, lt+1);
+               lt++;
+               i++;
+           }
+           else if (arr[i]>v){
+               swap(arr, i, gt-1);
+               gt--;
+           } else {
+               i++;
+           }
 
-        return j;
+       }
+       swap(arr, l , lt);
+
+
+        _quickSort(arr, l, lt-1);
+        _quickSort(arr, gt, r);
+
     }
+
 
     private static void swap(int[] arr, int i, int j){
         int temp = arr[i];
